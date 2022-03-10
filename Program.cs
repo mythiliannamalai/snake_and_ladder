c8369a0e6756411bd48541c4b00e198e1f00e778
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-10 19:00:10 +0530

    Final report

--- Program.cs
+++ Program.cs
@@ -3,76 +3,197 @@
     public int DICE_VALUE = 0;
     public int PLAYER_ONE = 0;
     public int RAN_CHECK = 0;
-    public int DICE_ROLL_COUNT = 0;
-    
-
+    public int PLAYER_ONE_DICE_ROLL_COUNT = 0;
+    public int PLAYER_TWO = 0;
+    public int PLAYER_TWO_DICE_ROLL_COUNT = 0;
+    int U1 = 0;
+    int U2 = 0;
     public void method1()
     {
         Console.WriteLine("welcome to snake and ladder game");
-        if (PLAYER_ONE < 0)
+        if (PLAYER_ONE < 0 || PLAYER_TWO < 0)
         {
             Console.WriteLine("Restart the game...");
         }
         else
-        {
-            while (PLAYER_ONE < 100)
-            {
+        {            
+            while (PLAYER_ONE < 100 && PLAYER_TWO < 100)
+            {               
                 Random r1 = new Random();
                 DICE_VALUE = r1.Next(1, 7);
                 switch (DICE_VALUE)
                 {
                     case 1:
-                        Console.WriteLine(" \n dice value - 1");
-                        PLAYER_ONE = (PLAYER_ONE + 1);
+                        if (U1 == U2)                        
+                        {
+                            Console.WriteLine(" \n Player 1 dice value - 1");
+                            PLAYER_ONE = (PLAYER_ONE + 1);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 1");
+                            PLAYER_TWO = (PLAYER_TWO + 1);
+                        }
                         break;
                     case 2:
-                        Console.WriteLine(" \n dice value - 2");
-                        PLAYER_ONE = (PLAYER_ONE + 2);
+                        if (U1 == U2)
+                        {
+                            Console.WriteLine(" \n Player 1 dice value - 2");
+                            PLAYER_ONE = (PLAYER_ONE + 2);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 2");
+                            PLAYER_TWO = (PLAYER_TWO + 2);
+                        }
                         break;
                     case 3:
-                        Console.WriteLine("\n  dice value - 3");
-                        PLAYER_ONE = (PLAYER_ONE + 3);
+                        if (U1 == U2)
+                        {
+                            Console.WriteLine(" \n Player 1 dice value - 3");
+                            PLAYER_ONE = (PLAYER_ONE + 3);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 3");
+                            PLAYER_TWO = (PLAYER_TWO + 3);
+                        }
                         break;
                     case 4:
-                        Console.WriteLine("\n dice value -4");
-                        PLAYER_ONE = (PLAYER_ONE + 4);
+                        if (U1 == U2)
+                        {
+                            Console.WriteLine(" \n Player 1 dice value -4");
+                            PLAYER_ONE = (PLAYER_ONE + 4);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 4");
+                            PLAYER_TWO = (PLAYER_TWO + 4);
+                        }
                         break;
                     case 5:
-                        Console.WriteLine("\n dice value -5");
-                        PLAYER_ONE = (PLAYER_ONE + 5);
+                        if (U1 == U2)
+                        {
+                            Console.WriteLine(" \n Player 1 dice value -5");
+                            PLAYER_ONE = (PLAYER_ONE + 5);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 5");
+                            PLAYER_TWO = (PLAYER_TWO + 5);
+                        }
                         break;
                     default:
-                        Console.WriteLine("\n dice value - 6");
-                        PLAYER_ONE = (PLAYER_ONE + 6);
+                        if (U1 == U2)
+                        {
+                            Console.WriteLine(" \n Player 1 dice value - 6");
+                            PLAYER_ONE = (PLAYER_ONE + 6);
+                        }
+                        else
+                        {
+                            Console.WriteLine(" \n Player 2 dice value - 6");
+                            PLAYER_TWO = (PLAYER_TWO + 6);
+                        }
                         break;
                 }
-                DICE_ROLL_COUNT++;
                 if (PLAYER_ONE > 100)
                 {
                     PLAYER_ONE = (PLAYER_ONE - DICE_VALUE);
+                }
+                if (PLAYER_TWO > 100)
+                {
+                    PLAYER_TWO = (PLAYER_TWO - DICE_VALUE);
                 }
                 Random r2 = new Random();
                 RAN_CHECK = r2.Next(0, 3);
                 switch (RAN_CHECK)
                 {
                     case 0:
-                        Console.WriteLine(" NO PLAY ");
-
+                        if (U1 == U2)
+                        {
+                            PLAYER_ONE_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" NO PLAY ");
+                            Console.WriteLine("PLAYER 1: " + PLAYER_ONE);
+                            Console.WriteLine("dice roll count PLAYER 1 : " + PLAYER_ONE_DICE_ROLL_COUNT);
+                            U1++;
+                        }
+                        else
+                        {
+                            PLAYER_TWO_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" NO PLAY ");
+                            Console.WriteLine("PLAYER 2: " + PLAYER_TWO);
+                            Console.WriteLine("dice roll count PLAYER 2 : " + PLAYER_TWO_DICE_ROLL_COUNT);
+                            U2++;
+                        }
                         break;
                     case 1:
-                        Console.WriteLine(" LADDER");
+                        if (U1 == U2)
+                        {
+                            PLAYER_ONE_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" LADDER");
+                            Console.WriteLine("PLAYER 1: " + PLAYER_ONE);
+                            Console.WriteLine("dice roll count PLAYER 1 : " + PLAYER_ONE_DICE_ROLL_COUNT);
+                            Random r3 = new Random();
+                            int SECOND_CHANCE = r3.Next(1, 7);
+                            PLAYER_ONE = (PLAYER_ONE + SECOND_CHANCE);
+                            if (PLAYER_ONE > 100)
+                            {
+                                PLAYER_ONE = (PLAYER_ONE - DICE_VALUE);
+                            }                            
+                            PLAYER_ONE_DICE_ROLL_COUNT++;
+                            Console.WriteLine("second chance value PLAYER 1 : " + PLAYER_ONE);
+                            Console.WriteLine("dice roll count PLAYER 1 : " + PLAYER_ONE_DICE_ROLL_COUNT);
+                            U1++;                            
+                        }
+                        else
+                        {
+                            PLAYER_TWO_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" LADDER");
+                            Console.WriteLine("PLAYER 2: " + PLAYER_TWO);
+                            Console.WriteLine("dice roll count PLAYER 2 : " + PLAYER_TWO_DICE_ROLL_COUNT);
+                            Random r3 = new Random();
+                            int SECOND_CHANCE = r3.Next(1, 7);
+                            PLAYER_TWO = (PLAYER_TWO + SECOND_CHANCE);                            
+                            if (PLAYER_TWO > 100)
+                            {
+                                PLAYER_TWO = (PLAYER_TWO - SECOND_CHANCE);
+                            }
+                            PLAYER_TWO_DICE_ROLL_COUNT++;
+                            Console.WriteLine("second chance value PLAYER 2 : " + PLAYER_TWO);
+                            Console.WriteLine("dice roll count PLAYER 2 : " + PLAYER_TWO_DICE_ROLL_COUNT);
+                            U2++;
+                        }
                         break;
                     default:
-                        Console.WriteLine(" SNAKE");
+                        if (U1 == U2)
+                        {
+                            PLAYER_ONE_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" SNAKE");
+                            Console.WriteLine("PLAYER 1: " + PLAYER_ONE);
+                            Console.WriteLine("dice roll count PLAYER 2 : " + PLAYER_ONE_DICE_ROLL_COUNT);
+                            U1++;
+                        }
+                        else
+                        {
+                            PLAYER_TWO_DICE_ROLL_COUNT++;
+                            Console.WriteLine(" SNAKE");
+                            Console.WriteLine("PLAYER 2 : " + PLAYER_TWO);
+                            Console.WriteLine("dice roll count PLAYER 2 : " + PLAYER_TWO_DICE_ROLL_COUNT);
+                            U2++;
+                        }
                         break;
                 }
-                Console.WriteLine("Player 1: " + PLAYER_ONE);
-                Console.WriteLine("dice roll count : " + DICE_ROLL_COUNT);
-
             }
         }
+        if(PLAYER_ONE==100)
+        {
+            Console.WriteLine(" \n \n *****PLAYER 1 WIN*****");
+        }
+        else
+        {
+            Console.WriteLine(" \n \n *****PLAYER 2 WIN*****");
+        }
     }
-   
 }
 class Final
 {
@@ -80,6 +201,6 @@ class Final
     {
         Gamer_S_L program_check = new Gamer_S_L();
         program_check.method1();
-
+        
     }
 }

05d45c09f157767b5d6cdffccd7b43ad6ac4e50a
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-10 13:17:47 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Gamer_S_L
+{
+    public void method1()
+    {
+        Console.WriteLine("welcome to snake and ladder game");
+    }
+}
+class Final
+{
+   public static void Main(string[] args)
+    {
+    Gamer_S_L program_check = new Gamer_S_L();
+        program_check.method1();
+    }
+}
